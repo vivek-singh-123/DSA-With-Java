0e1abde385cc37d58e89181395cb2b9f4129803e
Vivek Singh <singhvivek357480@gmail.com>
2025-07-30 18:43:52 +0530

    bubble sort inc/dec order

---
+++ src/Only_for_Practice/BubbleSort.java
@@ -0,0 +1,38 @@
+package Only_for_Practice;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {8, 7, 30, 4, 10, 6, 11, 20};
+        bubbleSort(arr);
+        for (int a : arr) {
+            System.out.print(a + " ");
+        }
+    }
+
+    static void bubbleSort(int[] arr) {
+        int len = arr.length;
+
+        //increasing order
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = 0; j < len - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        //decreasing order
+//        for (int i = 0; i < len - 1; i++) {
+//            for (int j = 0; j < len - i - 1; j++) {
+//                if (arr[j] < arr[j + 1]) {
+//                    int temp = arr[j+1];
+//                    arr[j+1] = arr[j];
+//                    arr[j] = temp;
+//                }
+//            }
+//        }
+
+    }
+}
