a00694ff6345848025f97c28d8ec8b249ba45f0c
Vivek Singh <singhvivek357480@gmail.com>
2025-07-31 23:56:46 +0530

    selection sort

--- src/Only_for_Practice/BubbleSort.java
+++ src/Only_for_Practice/BubbleSort.java
@@ -23,16 +23,6 @@ public class BubbleSort {
             }
         }
 
-        //decreasing order
-//        for (int i = 0; i < len - 1; i++) {
-//            for (int j = 0; j < len - i - 1; j++) {
-//                if (arr[j] < arr[j + 1]) {
-//                    int temp = arr[j+1];
-//                    arr[j+1] = arr[j];
-//                    arr[j] = temp;
-//                }
-//            }
-//        }
 
     }
 }
