be0ee5463c53662f6964e9e5706254afc4c92ca9
Vivek Singh <singhvivek357480@gmail.com>
2025-08-05 18:40:18 +0530

    quick sort

---
+++ src/Only_for_Practice/QuickSort.java
@@ -0,0 +1,45 @@
+package Only_for_Practice;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr={10,30,70,100,20,80,40,90,50,60};
+        int len=arr.length;
+        quickSort(arr, 0, len-1);
+
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+
+    }
+
+    public static void quickSort(int[] arr, int low, int high){
+        if (low<high){
+            int pi=partition(arr, low, high);
+
+            quickSort(arr, low, pi-1);
+            quickSort(arr, pi+1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high){
+        int pivot=arr[high];
+        int i=low-1;
+
+        for(int j=low; j<high; j++){
+            if (arr[j]<pivot){
+                i++;
+                //swap
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+
+        int temp=arr[i+1];
+        arr[i+1]=arr[high];
+        arr[high]=temp;
+
+        return i+1;
+    }
+}
+
