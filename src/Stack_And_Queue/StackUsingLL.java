58cae816f6ae85bf9cd726724ac0506551d56bd9
Vivek Singh <singhvivek357480@gmail.com>
2025-08-10 15:58:07 +0530

    stack using linkedlist

---
+++ src/Stack_And_Queue/StackUsingLL.java
@@ -0,0 +1,63 @@
+package Stack_And_Queue;
+
+public class StackUsingLL {
+    public static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+
+    public static class Stack{
+        private static Node head;
+
+        public static boolean isEmpty(){
+            return head == null;
+        }
+
+        public static void push(int data){
+            Node newNode=new Node(data);
+
+            if(isEmpty()){
+                head=newNode;
+                return;
+            }
+            newNode.next=head;
+            head=newNode;
+        }
+
+        public static int pop(){
+            if(isEmpty()){
+                return -1;
+            }
+
+            int top = head.data;
+            head=head.next;
+            return top;
+        }
+
+        public static int peek(){
+            if(isEmpty()){
+                return -1;
+            }
+
+            return head.data;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Stack s=new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
