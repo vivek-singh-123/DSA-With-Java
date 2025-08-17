2381715991fda4841527c6d9c4ee67ba674ada04
Vivek Singh <singhvivek357480@gmail.com>
2025-08-17 13:52:34 +0530

    postfix evaluation

---
+++ src/Stacks/PostfixEvaluation.java
@@ -0,0 +1,28 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class PostfixEvaluation {
+    public static void main(String[] args) {
+        String postfix = "953+4*6/-";
+        Stack<Integer> st=new Stack<>();
+
+        for(int i=0; i<postfix.length(); i++){
+            char ch=postfix.charAt(i);
+
+            int ascii=(int)ch;
+            if(ascii>=48 && ascii<=57){
+                st.push(ascii-48);
+            }else if(ch=='-' || ch=='+' || ch=='*' || ch=='/'){
+                int v2=st.pop();
+                int v1=st.pop();
+
+                if(ch=='-') st.push(v1-v2);
+                if(ch=='+') st.push(v1+v2);
+                if(ch=='*') st.push(v1*v2);
+                if(ch=='/') st.push(v1/v2);
+            }
+        }
+        System.out.println(st.peek());
+    }
+}
