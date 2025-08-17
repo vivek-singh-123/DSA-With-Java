3c02826ce599742c070790652dcac30e48514515
Vivek Singh <singhvivek357480@gmail.com>
2025-08-17 17:15:28 +0530

    prefix to infix conversion

--- src/Stacks/PrefixToInfix.java
+++ src/Stacks/PrefixToInfix.java
@@ -2,20 +2,20 @@ package Stacks;
 
 import java.util.Stack;
 
-public class PostfixToInfix {
+public class PrefixToInfix {
     public static void main(String[] args) {
-        String postFix="953+4*6/-";
+        String preFix="-9/*+5346";
         Stack<String> val=new Stack<>();
 
-        for(int i=0; i<postFix.length(); i++){
-            char ch=postFix.charAt(i);
-            int ascii=(int)ch;
+        for(int i=preFix.length()-1; i>=0; i--){
+            char ch=preFix.charAt(i);
 
-            if (ascii>=48 && ascii<=57){
+            int ascii=(int)ch;
+            if(ascii>=48 && ascii<=57){
                 val.push(String.valueOf(ascii-48));
-            }else {
-                String val2=val.pop();
+            } else {
                 String val1=val.pop();
+                String val2=val.pop();
                 char op=ch;
 
                 String s="(" + val1 + op + val2 + ")";
