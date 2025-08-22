a6b1d9e89d4a9fd6688916a1ba2be7ce5a11529d
Vivek Singh <singhvivek357480@gmail.com>
2025-08-22 15:54:18 +0530

    normal queue basic op

---
+++ src/Queue/queueImplementationUsingArray.java
@@ -0,0 +1,69 @@
+package Queue;
+
+public class queueImplementationUsingArray {
+
+    public static class queue{
+        static int[] arr;
+        static int size;
+        static int rear=-1;
+
+        queue(int n){
+            arr=new int[n];
+            this.size=n;
+        }
+
+        //check empty
+        public static boolean isEmpty(){
+            return rear == -1;
+        }
+
+        //enqueue(add)
+        public static void add(int data){
+            if(rear == size-1){
+                System.out.println("queue size is full!");
+                return;
+            }
+
+            rear++;
+            arr[rear]=data;
+        }
+
+        //dequeue(remove)
+        public static int remove(){
+            if(isEmpty()){
+                System.out.println("queue is empty!");
+                return -1;
+            }
+
+            int front=arr[0];
+            for(int i=0; i<rear; i++){
+                arr[i]=arr[i+1];
+            }
+            rear--;
+            return front;
+        }
+
+        //peek
+        public static int peek(){
+            if (isEmpty()){
+                System.out.println("queue is empty!");
+                return -1;
+            }
+
+            return arr[0];
+        }
+
+    }
+    public static void main(String[] args) {
+        queue q=new queue(5);
+        q.add(2);
+        q.add(5);
+        q.add(7);
+        q.add(11);
+
+        while (!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
