7255a00e6999acf94ab50733e717be8d00c92513
Vivek Singh <singhvivek357480@gmail.com>
2025-08-27 11:43:09 +0530

    print negative no without using '-' singn

---
+++ src/BitManipulation/printNegativeNo.java
@@ -0,0 +1,12 @@
+package BitManipulation;
+
+public class printNegativeNo {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(n);
+
+        n = ~n;
+        n = n+1;
+        System.out.println(n);
+    }
+}
