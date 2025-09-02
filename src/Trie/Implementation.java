15a3a675295c3ee14f5e7a7f8e051d8e9fe7dd00
Vivek Singh <singhvivek357480@gmail.com>
2025-09-02 13:20:47 +0530

    add search function & word break problem

--- src/Trie/Implementation.java
+++ src/Trie/Implementation.java
@@ -21,28 +21,75 @@ public class Implementation {
 
     //insert
     static void insert(String word){
+        Node curr=root;
         for(int i=0; i<word.length(); i++){  //T.C would be O(L)
             int idx = word.charAt(i)-'a';
 
-            if(root.children[idx] == null){
-                root.children[idx]=new Node();
+            if(curr.children[idx] == null){
+                curr.children[idx]=new Node();
             }
 
             if(i == word.length()-1){
-                root.children[idx].eow=true;
+                curr.children[idx].eow=true;
             }
 
-            root=root.children[idx];
+            curr=curr.children[idx];
         }
     }
 
     //search
+    static boolean search(String key){
+        Node curr=root;
+        for(int i=0; i<key.length(); i++){
+            int idx=key.charAt(i) - 'a';
 
+            if(curr.children[idx] == null){
+                return false;
+            }
+
+            if(i == key.length()-1 && curr.children[idx].eow==false){
+                return false;
+            }
+
+            curr=curr.children[idx];
+        }
+        return true;
+    }
+
+    //word break problem
+    public static boolean wordBreak(String key){
+        if(key.isEmpty()){
+            return true;
+        }
+
+        for(int i=1; i<=key.length(); i++){
+            String firstPart = key.substring(0,i);
+            String secondPart = key.substring(i);
+
+            if(search(firstPart) && wordBreak(secondPart)){
+                return true;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
-        String[] words={"this", "is", "a", "trie", "data", "structure"};
+//        String[] words={"this", "is", "a", "trie", "data", "structure"};
 
+//        for(int i=0; i<words.length; i++){
+//            insert(words[i]);
+//        }
+
+//        System.out.println(search("their"));
+//        System.out.println(search("this"));
+//        System.out.println(search("structure"));
+//        System.out.println(search("an"));
+
+        String[] words = {"i", "like", "sam", "samsung", "mobile"};
         for(int i=0; i<words.length; i++){
             insert(words[i]);
         }
+
+        String key = "ilikesamsung";
+        System.out.println(wordBreak(key));
     }
 }
