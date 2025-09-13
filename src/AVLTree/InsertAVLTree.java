77fbad3f02decd60c79487e1bc4f4504ca8e91fa
Vivek Singh <singhvivek357480@gmail.com>
2025-09-13 23:16:24 +0530

    insert AVL tree

---
+++ src/AVLTree/InsertAVLTree.java
@@ -0,0 +1,19 @@
+package AVLTree;
+
+public class InsertAVLTree {
+    public static class Node{
+        int val;
+        Node left;
+        Node right;
+        int height;
+
+        Node(int val){
+            this.val=val;
+            left = right = null;
+            height = 1;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
