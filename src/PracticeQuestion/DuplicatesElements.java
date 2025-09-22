6f0bee5b8dc5f299a5ae6ef226431c398f6b81d8
Vivek Singh <singhvivek357480@gmail.com>
2025-09-23 00:00:04 +0530

    duplicates found

---
+++ src/PracticeQuestion/DuplicatesElements.java
@@ -0,0 +1,20 @@
+package PracticeQuestion;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// Find Duplicate Elements in an Array
+public class DuplicatesElements {
+    public static void main(String[] args) {  // T.C ==> O(n)
+        int[] arr = {1, 2, 3, 2, 4, 1, 5};
+        Set<Integer> st=new HashSet<>();
+
+        for(int a : arr){
+            if(st.contains(a)){
+                System.out.print(a+" ");
+            }
+            st.add(a);
+        }
+
+    }
+}
