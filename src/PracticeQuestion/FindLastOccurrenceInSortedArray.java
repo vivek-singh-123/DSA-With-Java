a3e95601182c7130c1cee5aae21a98b7ba37c989
Vivek Singh <singhvivek357480@gmail.com>
2025-09-24 19:19:02 +0530

    last index of target in sorted array with duplicates

---
+++ src/PracticeQuestion/FindLastOccurrenceInSortedArray.java
@@ -0,0 +1,31 @@
+package PracticeQuestion;
+
+//Find last index of target in sorted array with duplicates.
+public class FindLastOccurrenceInSortedArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,2,3};
+        int target  = 2;
+        int result = -1;
+        int start = 0, end = arr.length-1;
+
+        while (start <= end){
+            int mid = (start+end)/2;
+
+            if(arr[mid] == target){
+                result = mid;
+                start = mid+1;
+            }
+            else if(target > arr[mid]){
+                start = mid+1;
+            } else {
+                end = mid-1;
+            }
+        }
+
+        if(result  != -1){
+            System.out.println("last occurrence at index: "+result);
+        }else {
+            System.out.println("target not found!!!!!!!!");
+        }
+    }
+}
