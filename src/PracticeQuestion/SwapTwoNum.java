66b212dd59b33296246faeca0f2ab3b6f367fd5e
Vivek Singh <singhvivek357480@gmail.com>
2025-09-25 16:37:53 +0530

    swap two num without third variable

---
+++ src/PracticeQuestion/SwapTwoNum.java
@@ -0,0 +1,18 @@
+package PracticeQuestion;
+
+public class SwapTwoNum {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+
+//        a = a+b;
+//        b = a-b;
+//        a = a-b;
+//        System.out.println(a+" "+b);
+
+        a = a^b;
+        b = a^b;
+        a = a^b;
+        System.out.println(a+" "+b);
+    }
+}
