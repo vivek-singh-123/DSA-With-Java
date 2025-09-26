fbe1bf26771c614d93fc06a4084b4d1c1c2fab3d
Vivek Singh <singhvivek357480@gmail.com>
2025-09-26 14:02:41 +0530

    check balanced parentheses

---
+++ src/PracticeQuestion/balancedParentheses.java
@@ -0,0 +1,32 @@
+package PracticeQuestion;
+
+import java.util.Stack;
+
+public class balancedParentheses {
+    public static boolean isBalanced(String str){
+        Stack<Character> st=new Stack<>();
+        int n = str.length();
+
+        for(int i=0; i<n; i++){
+            char ch = str.charAt(i);
+            if(ch == '('){
+                st.push(ch);
+            }else if(ch == ')'){
+                if(st.isEmpty()){
+                    return false;
+                }else {
+                    if(st.peek() == '('){
+                        st.pop();
+                    }
+                }
+            }
+        }
+
+        return st.isEmpty();
+    }
+    public static void main(String[] args) {
+        String str = "()(())()()((()))";
+
+        System.out.println(isBalanced(str));
+    }
+}
