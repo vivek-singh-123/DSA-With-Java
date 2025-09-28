48d4cec03e7ab19d29d0248eeb3a07ff18501799
Vivek Singh <singhvivek357480@gmail.com>
2025-09-28 18:58:26 +0530

    kth smallest element

--- src/PracticeQuestion/KthSmallestElement.java
+++ src/PracticeQuestion/KthSmallestElement.java
@@ -1,19 +1,20 @@
 package PracticeQuestion;
 
+import java.util.Collections;
 import java.util.PriorityQueue;
 
-public class KthLargestElement {
+public class KthSmallestElement {
     public static void main(String[] args) {
         int[] arr = {2,15,3,11,7,10};
         int k = 3;
 
-        PriorityQueue<Integer> pq=new PriorityQueue<>();  //min heap
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());  //max heap
         for(int i=0; i<k; i++){
             pq.add(arr[i]);
         }
 
         for(int i=k; i< arr.length; i++){
-            if(arr[i] > pq.peek()){
+            if(arr[i] < pq.peek()){
                 pq.poll();
                 pq.offer(arr[i]);
             }
