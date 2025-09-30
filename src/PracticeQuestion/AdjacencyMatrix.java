a656bc5947abe3a229f47e4f9eb76fd38ee2349b
Vivek Singh <singhvivek357480@gmail.com>
2025-10-01 02:17:38 +0530

    adjacency matrix

---
+++ src/PracticeQuestion/AdjacencyMatrix.java
@@ -0,0 +1,36 @@
+package PracticeQuestion;
+
+import java.util.Scanner;
+
+public class AdjacencyMatrix {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("enter no of vertex: ");
+        int V = sc.nextInt();
+
+        System.out.print("enter no of edges: ");
+        int E = sc.nextInt();
+
+        int[][] adjacencyMatrix = new int[V][V];
+
+        System.out.println("enter edges in (u v) format:");
+
+        for(int i=0; i<E; i++){
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+
+            adjacencyMatrix[u][v] = 1;
+            adjacencyMatrix[v][u] = 1;
+        }
+
+        for(int i=0; i<V; i++){
+            for(int j=0; j<V; j++){
+                System.out.print(adjacencyMatrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
